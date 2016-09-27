4aa605461828c47f4b980ef269db1e2dc62e4c30
My MegaDevelTest <fordevel@outlook.com>
2016-09-27 20:27:32 +0200

    main

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.TextBox();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(28, 28);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(142, 20);
+            this.input.TabIndex = 0;
+            // 
+            // btnInput
+            // 
+            this.btnInput.Location = new System.Drawing.Point(28, 54);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(142, 23);
+            this.btnInput.TabIndex = 1;
+            this.btnInput.Text = "Convert to basePrime";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(197, 28);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(305, 52);
+            this.output.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 101);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.btnInput);
+            this.Controls.Add(this.input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.Label output;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public int[] aPrimes = new int[] { 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73 };
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            int inputValue;
+            if (!int.TryParse(input.Text, out inputValue))
+            {
+                output.Text = "Sorry dude, I can't work with that.  Try a number.";
+            }
+            else {
+
+                //int inputValue = Convert.ToInt16(input.Text);
+                string tempOutput = "";
+
+                //check for incorrect input
+                // not allowed: negative & 0
+                if (inputValue <= 0) output.Text = "Sorry dude, I can only work with positive numbers.  Try a different number.";
+
+                // check if input is in table & return
+                int index = Array.IndexOf(aPrimes, inputValue);
+                if (index >= 0) tempOutput = "(" + Convert.ToString(index) + ")";
+
+                //else, start building the value
+                else
+                {
+                    int subtotal = inputValue;
+                    int divider = 2; //start with the lowest prime
+
+                    // get hightest prime < input
+                    int highestPrime = getHighestPrime(inputValue);
+
+                    while (divider < highestPrime && subtotal > 1)
+                    {
+                        if (subtotal % divider == 0)
+                        {
+                            subtotal = subtotal / divider;
+                            tempOutput = tempOutput + "(" + divider + ")";
+                        }
+                        else divider = getNextPrime(divider);
+                    }
+                }
+
+
+                output.Text = Convert.ToString(tempOutput);
+            }
+        }
+
+        public int getHighestPrime(int matchValue)
+        {
+            int maxVal = 0;
+            int index = -1;
+
+            for (int i = 0; i < aPrimes.Length; i++)
+            {
+                int thisNum = aPrimes[i];
+                if ((thisNum > maxVal) && thisNum < matchValue)
+                {
+                    maxVal = thisNum;
+                    index = i;
+                }
+            }
+
+            return maxVal;
+        }
+
+        public int getNextPrime(int matchValue)
+        {
+            int index = Array.IndexOf(aPrimes, matchValue);
+            //TODO: opvangen, als de volgende niet bestaat.
+            return aPrimes[index + 1];
+        }
+    }
+}
